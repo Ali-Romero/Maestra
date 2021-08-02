64460a61c420a27fa3f3ae1ae3cfa7841ec1846c
Sam <sherali920825@yandex.ru>
2021-08-02 18:56:38 +0600

    modal-rebuild

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
+  // menu
   $('.menu-burger').click(function () {
     $(this).toggleClass('active')
+  });
+
+  // slider
+  $('.js-portfolio-slider').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+  })
+
+  // modal slider
+  $(document).on('opening', '.js-projects-modal', function () {
+    $('.js-projects-slider').slick({
+      dots: false,
+      arrows: true,
+      infinite: true,
+    })
+  });
+
+  $(document).on('closed', '.remodal', function (e) {
+    $('.js-projects-slider').slick('unslick')
   });
 })
